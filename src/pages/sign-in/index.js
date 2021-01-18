b6da2193545ff19e6bbc4a344f692e97efcb6a1e
James Nuanez <james@jamesnuanez.com>
2021-01-17 20:30:44 -0800

    Implement basic email sign-in

--- src/pages/sign-in/index.js
+++ src/pages/sign-in/index.js
@@ -9,43 +9,43 @@ import Input from '../../components/elements/Input';
 import PasswordInput from '../../components/elements/PasswordInput';
 import FormError from '../../components/blocks/FormError';
 
-const Login = props => {
+const SignIn = props => {
   let history = useHistory();
   useEffect(() => {
-    document.title = `Login | Jambonz | Open Source CPAAS`;
+    document.title = `Sign In | Jambonz`;
   });
 
   // Refs
-  const refUsername = useRef(null);
+  const refEmail = useRef(null);
   const refPassword = useRef(null);
 
   // Form inputs
-  const [ username,     setUsername     ] = useState('');
+  const [ email,        setEmail        ] = useState('');
   const [ password,     setPassword     ] = useState('');
   const [ errorMessage, setErrorMessage ] = useState('');
 
   // Invalid form inputs
-  const [ invalidUsername, setInvalidUsername ] = useState(false);
+  const [ invalidEmail, setInvalidEmail ] = useState(false);
   const [ invalidPassword, setInvalidPassword ] = useState(false);
 
   const handleSubmit = async e => {
     try {
       e.preventDefault();
       setErrorMessage('');
-      setInvalidUsername(false);
+      setInvalidEmail(false);
       setInvalidPassword(false);
 
-      if (!username && !password) {
-        setErrorMessage('Username and password are required');
-        setInvalidUsername(true);
+      if (!email && !password) {
+        setErrorMessage('Email and password are required');
+        setInvalidEmail(true);
         setInvalidPassword(true);
-        refUsername.current.focus();
+        refEmail.current.focus();
         return;
       }
-      if (!username) {
-        setErrorMessage('Username is required');
-        setInvalidUsername(true);
-        refUsername.current.focus();
+      if (!email) {
+        setErrorMessage('Email is required');
+        setInvalidEmail(true);
+        refEmail.current.focus();
         return;
       }
 
@@ -56,145 +56,43 @@ const Login = props => {
         return;
       }
 
-      // Log in
+      // Sign in
       const response = await axios({
         method: 'post',
         baseURL: process.env.REACT_APP_API_BASE_URL,
-        url: '/login',
-        data: { username, password },
+        url: '/signin',
+        data: { email, password },
       });
 
-      // New account, password change required
-      if (response.data.force_change) {
-        // `user_sid` and `old_password` are needed for the new password form.
-        // They're saved to sessionStorage so that the data does not persist.
-        sessionStorage.setItem('user_sid', response.data.user_sid);
-        sessionStorage.setItem('old_password', password);
-        history.push('/create-password');
-        return;
-      }
+      localStorage.setItem('jwt', response.data.jwt);
+      localStorage.setItem('user_sid', response.data.user_sid);
+      localStorage.setItem('provider', response.data.provider);
 
-      // Save API key
-      if (response.data.token) {
-        localStorage.setItem('token', response.data.token);
-      }
-
-      //-----------------------------------------------------------------------------
-      // Get account data
-      //-----------------------------------------------------------------------------
-      const serviceProvidersPromise = axios({
-        method: 'get',
-        baseURL: process.env.REACT_APP_API_BASE_URL,
-        url: '/serviceProviders',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      });
-
-      const accountsPromise = axios({
-        method: 'get',
-        baseURL: process.env.REACT_APP_API_BASE_URL,
-        url: '/Accounts',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      });
-
-      const applicationsPromise = axios({
-        method: 'get',
-        baseURL: process.env.REACT_APP_API_BASE_URL,
-        url: '/applications',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      });
-
-      const voipCarriersPromise = axios({
-        method: 'get',
-        baseURL: process.env.REACT_APP_API_BASE_URL,
-        url: '/voipCarriers',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      });
-
-      const promiseAllValues = await Promise.all([
-        serviceProvidersPromise,
-        accountsPromise,
-        applicationsPromise,
-        voipCarriersPromise,
-      ]);
-
-      const serviceProviders = promiseAllValues[0].data;
-      const accounts         = promiseAllValues[1].data;
-      const applications     = promiseAllValues[2].data;
-      const voipCarriers     = promiseAllValues[3].data;
-
-      //-----------------------------------------------------------------------------
-      // Determine where to route user
-      //-----------------------------------------------------------------------------
-      if (
-        (serviceProviders.length > 1) ||
-        (accounts.length         > 1) ||
-        (accounts.length         < 1) ||
-        (applications.length     > 1) ||
-        (voipCarriers.length     > 0)
-      ) {
-        history.push('/internal/accounts');
-        return;
-      }
-
-      const { sip_realm, registration_hook } = accounts[0];
-
-      if (
-        (!sip_realm || !registration_hook) &&
-        !applications.length
-      ) {
-        history.push('/configure-account');
-        return;
-      }
-
-      if (!applications.length) {
-        history.push('/create-application');
-        return;
-      }
-
-      history.push('/internal/accounts');
+      history.push('/account');
 
     } catch (err) {
-      // 400 --> one or both fields are empty (prevented by above error checking)
-      // 403 --> username or password are incorrect
-      if (
-        err.response
-        && err.response.status
-        && err.response.status > 399
-        && err.response.status < 500
-      ) {
-        setErrorMessage('Login credentials are incorrect');
-      } else {
-        setErrorMessage(
-          (err.response && err.response.data && err.response.data.msg) ||
-          'Something went wrong, please try again.'
-        );
-        console.log(err.response || err);
-      }
+      setErrorMessage(
+        (err.response && err.response.data && err.response.data.msg) ||
+        'Username and/or password are incorrect.'
+      );
+      console.error(err.response || err);
     }
   };
 
   return (
-    <SetupTemplate title="Log In">
-      <Form onSubmit={handleSubmit}>
+    <SetupTemplate title="Sign In">
+      <Form left onSubmit={handleSubmit}>
         <Input
           large
           fullWidth
-          type="text"
-          name="username"
-          id="username"
-          placeholder="Username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-          ref={refUsername}
-          invalid={invalidUsername}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          ref={refEmail}
+          invalid={invalidEmail}
           autoFocus
         />
         <PasswordInput
@@ -216,7 +114,7 @@ const Login = props => {
           large
           fullWidth
         >
-          Log In
+          Sign In
         </Button>
         <Link to="/register">Register</Link>
       </Form>
@@ -224,4 +122,4 @@ const Login = props => {
   );
 };
 
-export default Login;
+export default SignIn;
