7d8faca228de1b4ad406d457e323cb8ccfdbfb6d
love1119 <goldenstar007.dev@gmail.com>
2021-03-03 22:26:31 +0200

    update page for deleting api keys

--- src/pages/account/api-keys/delete.js
+++ src/pages/account/api-keys/delete.js
@@ -1,12 +1,18 @@
 import InternalMain from '../../../components/wrappers/InternalMain';
-import Link from '../../../components/elements/Link';
 import Section from '../../../components/blocks/Section';
+import P from '../../../components/elements/P';
+import Button from '../../../components/elements/Button';
+import InputGroup from '../../../components/elements/InputGroup';
 
 const ApiKeyDelete = () => {
   return (
-    <InternalMain title="Delete API Key">
+    <InternalMain title="Delete API Key" topMenu={{ label: "← Back to Account Home", link: "/account" }}>
       <Section>
-        <Link to="/account/api-keys/test">Go to API Key Details</Link>
+        <P>Are you sure you want to delete this API key? This cannot be undone, but you can always create new API keys.</P>
+        <InputGroup flexEnd spaced>
+          <Button gray="true">Cancel</Button>
+          <Button>Delete</Button>
+        </InputGroup>
       </Section>
     </InternalMain>
   );
