5235ab715d6318c2128e5f45385fe59e29c97106
love1119 <goldenstar007.dev@gmail.com>
2021-03-11 22:31:41 +0200

    fix invoice date issue

--- src/components/blocks/Subscription.js
+++ src/components/blocks/Subscription.js
@@ -71,9 +71,9 @@ const Subscription = ({ data, hasDelete }) => {
         setInvoiceData({
           total: result.data.total / 100,
           currency: result.data.currency,
-          next_payment_attempt: moment(result.data.next_payment_attempt).format(
-            "MMMM DD, YYYY"
-          ),
+          next_payment_attempt: moment(
+            result.data.next_payment_attempt * 1000
+          ).format("MMMM DD, YYYY"),
         });
       } catch (err) {
         console.log(err);
