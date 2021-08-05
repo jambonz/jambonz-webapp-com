d43eab5ee391b8081a5948e418f8f38d806d844a
Dave Horton <daveh@beachdognet.com>
2021-08-05 16:55:18 -0400

    bugfix: dont request unnecessary scope from google

--- src/pages/account/settings/auth/index.js
+++ src/pages/account/settings/auth/index.js
@@ -65,7 +65,7 @@ const SettingsAuthIndex = () => {
   localStorage.setItem("location-before-oauth", "/account/settings/auth");
   localStorage.setItem("oauth-state", state);
   const gitHubUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&state=${state}&scope=user:email&allow_signup=false`;
-  const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=email+profile+https://www.googleapis.com/auth/cloud-platform&access_type=offline&include_granted_scopes=true&response_type=code&state=${state}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}`;
+  const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=email+profile&access_type=offline&include_granted_scopes=true&response_type=code&state=${state}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}`;
 
   const authenticationMethods = ["github", "google", "local"];
 
